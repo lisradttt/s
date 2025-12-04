b6a544be8f689b0c75c29a1814746abda4db65b9
Malak Ahmed <miro20m20m20@gmail.com>
2025-12-05 00:42:45 +0100

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,12 @@
+# متطلبات التنصيب #
+OWNER = ["ISIIQ", "e2zzz"]
+OWNER_NAME = "MAMIUS"
+BOT_TOKEN = "8355863539:AAEBEmnyySm6fVrgB-A4pYMvR91sabfwK74"
+DATABASE = "mongodb+srv://MiMIUS:MiMIUS@cluster0.onagyhb.mongodb.net/?appName=Cluster0"
+CHANNEL = "https://t.me/E_BBN"
+GROUP = "https://t.me/JoyBoyIQ"
+VIDEO = "https://files.catbox.moe/fp1rxu.jpg"
+PHOTO = "https://files.catbox.moe/fp1rxu.jpg"
+LOGS = "ISIIQ"
+
+
